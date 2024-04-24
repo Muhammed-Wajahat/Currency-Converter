0225abed3251a8ffea547df2fa4cb2412091f437
Wajahat Ali <wajahatalisadique@gmail.com>
2024-04-24 08:26:48 -0700

    New Project

---
+++ README.md
@@ -0,0 +1,8 @@
+"# Currency-Converter" 
+🚀WellCome to Currency Counverter🚀.
+Hi Coders this CLI Base Currency Counverter.
+Using
+Typescript.
+Node.js
+Inquirer
+command:npx w6rw-currency-exchange
---
+++ app.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let convertion = {
+  PKR: {
+    USD: 0.0036,
+    GBP: 0.0029,
+    PKR: 1,
+  },
+  GBP: {
+    USD: 1.24,
+    PKR: 344.57,
+    GBP: 1,
+  },
+  USD: {
+    PKR: 278.13,
+    GBP: 0.81,
+    USD: 1,
+  },
+};
+
+const answer: {
+  form: "PKR" | "USD" | "GBP";
+  to: "PKR" | "USD" | "GBP";
+  amount: number;
+} = await inquirer.prompt([
+  {
+    name: "form",
+    type: "list",
+    message: "please enter a your currency:",
+    choices: ["PKR", "USD", "GBP"],
+  },
+  {
+    name: "to",
+    type: "list",
+    message: "please enter a your covertion currency:",
+    choices: ["PKR", "USD", "GBP"],
+  },
+  {
+    name: "amount",
+    type: "input",
+    message: "Enter a your covertion amount:",
+  },
+]);
+const { form, to, amount } = answer;
+if (form && to && amount) {
+  let result = convertion[form][to] * amount;
+  console.log(`your converstion from ${form} to ${to} is: ${result}`);
+} else {
+  console.log("Invalid input");
+}
